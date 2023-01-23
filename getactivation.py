2c3d024964ec748b7e2fb2020e62094ee416fc49
Zou Yanze <91880399+zouyanze@users.noreply.github.com>
2023-01-23 13:06:06 +0900

    Create getactivation.py

--- getactivation.py
+++ getactivation.py
@@ -3,8 +3,21 @@ from transformers import BertTokenizer
 from model import BertForMultiLabelClassification
 from multilabel_pipeline import MultiLabelPipeline
 from pprint import pprint
+import torch
 tokenizer = BertTokenizer.from_pretrained("monologg/bert-base-cased-goemotions-original")
 model = BertForMultiLabelClassification.from_pretrained("monologg/bert-base-cased-goemotions-original")
 
-inputs =  tokenizer("Hello!", return_tensors= "pt")
-outputs = model(**inputs)
+activation = {}
+def get_activation(name):
+    def hook(model, input, output):
+        activation[name] = output.detach()
+    return hook
+
+model.bert.register_forward_hook(get_activation('bert')) #layer problem
+x = #input problem
+output = model(x)
+print(activation['bert'])
+
+
+#output_attention = True
+  ->
